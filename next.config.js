08ef728c33c2799d1411abd96e12ba543a3dfd72
Lucas Huls <lucas@mylock.nl>
2026-08-13 15:32:10 +0200

    Fix service worker precaching failure for app-build-manifest.json

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   register: false,
   skipWaiting: true,
+  buildExcludes: [/app-build-manifest\.json$/],
 })
 
 /** @type {import('next').NextConfig} */
